00b9cc24012f91f89e49fe4403e994e129371ebb
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-02 01:27:15 +0600

    Update DOMManipulation.js

--- src/DOMManipulation.js
+++ src/DOMManipulation.js
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DOM Manipulation</title>
+</head>
+<body>
+    <h1 id="header">Original Text</h1>
+    <button onclick="changeText()">Change Text</button>
+
+    <script>
+        function changeText() {
+            document.getElementById("header").innerHTML = "New Text";
+        }
+    </script>
+</body>
+</html>
 
