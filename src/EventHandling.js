d257dc3c75f50504e901e527cb50af8c472ebb9e
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-02 01:27:07 +0600

    Update EventHandling.js

--- src/EventHandling.js
+++ src/EventHandling.js
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Event Handling</title>
+</head>
+<body>
+    <button id="myButton">Click Me</button>
+    <script>
+        document.getElementById("myButton").addEventListener("click", function() {
+            alert("Button clicked!");
+        });
+    </script>
+</body>
+</html>
 
