dc8ad8a832ecb5d97ff9fdabea685c59ffec2db0
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-02 01:26:57 +0600

    Update AsyncAwaitExample.js

--- src/AsyncAwaitExample.js
+++ src/AsyncAwaitExample.js
@@ -1,1 +1,15 @@
+function getData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Data received");
+        }, 1000);
+    });
+}
+
+async function fetchData() {
+    let data = await getData();
+    console.log(data);
+}
+
+fetchData();
 
