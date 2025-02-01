bfe6ec796cb399d4d317d339a2920a7041818777
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-02 01:26:25 +0600

    Update ObjectMethodsExample.js

--- src/ObjectMethodsExample.js
+++ src/ObjectMethodsExample.js
@@ -1,1 +1,9 @@
-StringManipulation.js
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    start() {
+        console.log(`The ${this.brand} ${this.model} is starting.`);
+    }
+};
+
+car.start();
