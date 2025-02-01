d658dc6772b7ece35b4c4de4ab6212152ee04fc8
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-02 01:24:54 +0600

    Update SwitchExample.js

--- src/SwitchExample.js
+++ src/SwitchExample.js
@@ -1,1 +1,13 @@
+let day = 2;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("Invalid day");
+}
 
