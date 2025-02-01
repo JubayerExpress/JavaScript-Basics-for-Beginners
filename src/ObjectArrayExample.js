3724753c6ae2c779639296280e78187785e450bc
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-02 01:24:20 +0600

    Update ObjectArrayExample.js

--- src/ObjectArrayExample.js
+++ src/ObjectArrayExample.js
@@ -1,1 +1,9 @@
+let person = {
+    name: "John",
+    age: 30,
+    hobbies: ["Reading", "Traveling", "Sports"]
+};
+
+console.log(person.name); // Accessing object property
+console.log(person.hobbies[1]); // Accessing array inside object
 
